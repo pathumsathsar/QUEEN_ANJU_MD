21f45e348ab5ad6c17164d4e20582e575965bc8f
pathumsathsar <pethumsassara@gmail.com>
2024-12-07 20:30:59 +0530

    Update session.js

--- session.js
+++ session.js
@@ -52,5 +52,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=OA1iiIQD#vpV3yUTK-noMuTlc2K3Lbs0UpDNZhZsRFvUFZ1rGXqk",
 };
